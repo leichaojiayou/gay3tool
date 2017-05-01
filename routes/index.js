55bcfc4169a380e5ddd3ea3d363f123612a78301
472225640 <472225640@qq.com>
2017-05-01 17:03:24 +0800

    test

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/data',function(req,res,next){
+	res.send({msg: 'hello world'});
+});
+
 module.exports = router;
